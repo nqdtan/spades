22e2381d8fd2a622363de200c47294781e5865a3
nqdtan <tan.nqd@gmail.com>
2023-11-04 17:00:42 -0700

    [socket_cc] make sure all prerequisite ips are built

--- socket_cc_flow/run_flow.sh
+++ socket_cc_flow/run_flow.sh
@@ -6,6 +6,12 @@ project_name=project_socket_cc_${ts}
 sed -i "7i \`define EXCLUDE_RG_CL" ../rtl/socket_config.vh
 
 cd ../ulp_design
+rm -rf kernel_pack_ff_bridge
+make kernel_pack script=ff_bridge
+rm -rf kernel_pack_mbufgce_primitive
+make kernel_pack script=mbufgce_primitive
+rm -rf kernel_pack_lut1_primitive
+make kernel_pack script=lut_primitive
 rm -rf kernel_pack_socket_top
 make kernel_pack script=socket_top top=socket_top
 cd ../socket_cc_flow
@@ -18,4 +24,6 @@ if [ -f "$FILE" ]; then
   rm -rf ext && unzip socket_cc.dcp -d ext
   cd ext/ && rm *.ncr *.nts && cp ../../../../dcp.xml .
   zip socket_cc.dcp * && mv socket_cc.dcp ../
+  cp socket_cc.dcp ../../../../checkpoints/
+  cp socket_cc.edf ../../../../checkpoints/
 fi
